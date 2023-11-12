54f08a96ec2960cd3e1321a2c401ce90cbec3de6
Jaroslaw Glowka <jmglowka@protonmail.com>
2023-11-12 16:20:19 +0100

    Some ui work - changing the font etc.

--- scripts/health_bar.gd
+++ scripts/health_bar.gd
@@ -3,15 +3,23 @@ extends Control;
 
 @onready var health_bar_middle = $Control/HealthBarMiddle
 @onready var animation_player = $AnimationPlayer
+@onready var area := $Area2D;
+@onready var label := $Label;
 
 @export var max_health := 32;
 @export var animation_speed := 3.0;
 
 var _hp := max_health;
 
+func _ready() -> void:
+	area.mouse_entered.connect(entered);
+	area.mouse_exited.connect(exited);
+
 func set_health(hp: int, animate: bool = true) -> void:
 	var dx := 1.0 / float(max_health);
 	_hp = hp;
+
+	label.text = str(_hp) + " / " + str(max_health);
 
 	if animate:
 		var tween = get_tree().create_tween();
@@ -20,3 +28,11 @@ func set_health(hp: int, animate: bool = true) -> void:
 		animation_player.play("shake");
 	else:
 		health_bar_middle.region_rect.size.x = dx * _hp * 32;
+
+func entered() -> void:
+	print("entered");
+	label.visible = true;
+
+func exited() -> void:
+	print("exited");
+	label.visible = false;
